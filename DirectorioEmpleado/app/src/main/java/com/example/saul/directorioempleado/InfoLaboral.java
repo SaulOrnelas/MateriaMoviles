3a6d163fc80d33025d4fd6e83769833e6fdf8f26
SaulOrnelas <saulop97@outlook.com>
2019-04-03 23:13:35 -0600

    Avance de directorio empleado

---
+++ DirectorioEmpleado/app/src/main/java/com/example/saul/directorioempleado/InfoLaboral.java
@@ -0,0 +1,13 @@
+package com.example.saul.directorioempleado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoLaboral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_laboral);
+    }
+}
